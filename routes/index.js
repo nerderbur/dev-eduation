fef0adada9af855fecf078d98bfa5d8b1def410a
Leon Wright <wrightleon@icloud.com>
2016-09-12 15:25:32 -0500

    Changed Home Page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('/singleview', { title: 'Hiiii Gabrielle!!!! :)' });
 });
 
-router.get('/singleview', function (req, res, next) {
-  res.render('singleview', {title: 'Single View'});
-});
+// router.get('/singleview', function (req, res, next) {
+//   res.render('singleview', {title: 'Single View'});
+// });
 
 module.exports = router;
