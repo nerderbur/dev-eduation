31d4cd7d57f671c3651cdbe9d07fe762b97cb0f1
Leon Wright <wrightleon@icloud.com>
2016-09-12 15:27:05 -0500

    Fixed "/singleview" changed to "singleview"

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/singleview', { title: 'Hiiii Gabrielle!!!! :)' });
+  res.render('singleview', { title: 'Hiiii Gabrielle!!!! :)' });
 });
 
 // router.get('/singleview', function (req, res, next) {
